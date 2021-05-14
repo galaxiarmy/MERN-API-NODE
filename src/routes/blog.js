40dfbd85182674daa6a771f9c7ba4a0552e392f7
hanif <HanifPratamaa@gmail.com>
2021-05-14 19:14:20 +0700

    modified

---
+++ src/routes/blog.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const {body} = require('express-validator');
+
+const router = express.Router();
+
+const blogController = require('../controllers/blog')
+
+router.post('/post', [
+    body('title').isLength({min: 5}).withMessage('input title tidak sesuai'),
+    body('body').isLength({min: 5}).withMessage('input body tidak sesuai')], 
+    blogController.createBlogPost);
+
+module.exports = router
